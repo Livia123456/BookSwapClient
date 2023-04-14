3e4c37a73e19c2449c4cdf169d038b25c76647da
Livia123456 <122884168+Livia123456@users.noreply.github.com>
2023-04-14 14:54:29 +0200

    jdk

--- src/controller/ServerConnection.java
+++ src/controller/ServerConnection.java
@@ -1,4 +1,34 @@
 package controller;
 
-public class ServerConnection {
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+public class ServerConnection extends Thread{
+    private String ip;
+    private int port;
+    private ObjectOutputStream oos;
+    private ObjectInputStream ois;
+
+    public ServerConnection(String ip, int port) {
+        this.ip = ip;
+        this.port = port;
+        start();
+    }
+
+    @Override
+    public void run() {
+        try (Socket socket = new Socket(ip, port)) {
+            oos = new ObjectOutputStream(new DataOutputStream(socket.getOutputStream()));
+            ois = new ObjectInputStream()
+
+        } catch (UnknownHostException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
